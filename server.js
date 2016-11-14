353650bcb3016cae8edff2f93541ff16e1beca7f
Nguyen, Lyly <lyly.nguyen@accenture.com>
2016-11-13 16:58:42 -0800

    updated readme

--- server.js
+++ server.js
@@ -1,28 +1,10 @@
 
 // https://devcenter.heroku.com/articles/mongolab
 // http://todomvc.com/examples/angularjs/#/
-var express  = require('express'),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser'),
+var express  = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-    // Mongoose Schema definition
-    Schema = new mongoose.Schema({
-      id       : String, 
-      title    : String,
-      completed: Boolean
-    }),
-
-    Todo = mongoose.model('Todo', Schema);
-
-/*
- * I’m sharing my credential here.
- * Feel free to use it while you’re learning.
- * After that, create and use your own credential.
- * Thanks.
- *
- * MONGOLAB_URI=mongodb://example:example@ds053312.mongolab.com:53312/todolist
- * 'mongodb://example:example@ds053312.mongolab.com:53312/todolist'
- */
 mongoose.connect(process.env.MONGOLAB_URI, function (error) {
     if (error) console.error(error);
     else console.log('mongo connected');
