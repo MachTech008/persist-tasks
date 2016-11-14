735c112c56883811f2454b92a144982242d8a9c4
Nguyen, Lyly <lyly.nguyen@accenture.com>
2016-11-14 15:56:43 -0800

    updated server and package.json

--- server.js
+++ server.js
@@ -1,54 +1,127 @@
-
-// https://devcenter.heroku.com/articles/mongolab
-// http://todomvc.com/examples/angularjs/#/
 var express  = require('express');
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var request = require('request');
-
-var uri = process.env.MONGODB_URI || 'mongodb://lylyn:accenture1@ds151917.mlab.com:51917/persist-tasks';
-
-var Schema = new mongoose.Schema({
-    guid: String,
-    title: String,
-    username: String,
-    priority: String,
-    status: String,
-    description: String,
-    location: String,
-    creationTime: String,
-    deleted: Boolean
-});
-
-var Tasks = mongoose.model('Tasks', Schema);
-
-mongoose.connect(uri, function (error) {
- if (error) {
-  console.error(error);
- } else {
-  console.log('mongo connected');
- }
-});
+var fs = require('fs');
 
 var baseConnectWorkerURL = 'http://ec2-52-26-69-118.us-west-2.compute.amazonaws.com/api/v1/';
 
 express()
-  // https://scotch.io/tutorials/use-expressjs-to-get-url-and-post-parameters
-  .use(bodyParser.json()) // support json encoded bodies
-  .use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
-  
+  .use(bodyParser.json()) 
+  .use(bodyParser.urlencoded({ extended: true }))
+
   .get('/api', function (req, res) {
     res.json(200, {msg: 'OK'});
   })
-  
-  .get('/api/copytasks', function (req, res) {
-    request.get(baseConnectWorkerURL + 'tasks', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        body = JSON.parse(body);
-        for(var i = 0; i < body.length; i++) {
-          res.send(body);
+
+  .get('/api/getUser', function (req, res) {
+
+    request.get(baseConnectWorkerURL + 'users', function (error, response, body) {
+      body = JSON.parse(body);
+      if (body.indexOf('Laure Linn')) {
+        console.log('user found');
+
+      } else {
+        
+        console.log('user not found');
+
+        var update = {
+          url: baseConnectWorkerURL + 'users',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          json: {
+            name: 'Laure Linn',
+            credentials: 'DummyToken1' 
+          }
+        };
+
+        request(update, function (error, response, body) {
+          if (error) {
+            console.log(error);
+          } else {
+            res.send(body)
+          }
+        });
+      }
+    })
+  })
+
+  .get('/api/getCredentials', function (req, res) {
+    request.get(baseConnectWorkerURL + 'credentials', function (error, response, body) {
+      body = JSON.parse(body);
+
+      if (body.indexOf('DummyToken1')) {
+
+        console.log('credentials found');
+
+      } else {
+
+        console.log('credentials not found');
+
+        var update = {
+          url: baseConnectWorkerURL + 'credentials',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          json: {
+            token: 'DummyToken1'
+          }
+        };
+
+        request(update, function (error, response, body) {
+          if (error) {
+            console.log(error);
+          } else {
+            res.send(body);
+          }
+        })
+      }
+    })
+  })
+
+  .get('/api/getLocations' , function (req, res) {
+    request.get(baseConnectWorkerURL + 'locations', function (error, response, body) {
+      body = JSON.parse(body);
+      var rooms = [];
+
+      for (var i = 0; i < body.length; i++) {
+        rooms.push(body[i].name)
+
+        if(!rooms.indexOf('101') || (!rooms.indexOf('102')) || (!rooms.indexOf('103')) || (!rooms.indexOf('104'))) {
+          
+          console.log(error);
+          var locations = ['101', '102', '103', '104'];
+          
+          for (var i = 0; i < locations.length; i++ ){
+            
+            var update = {
+              url: baseConnectWorkerURL + 'locations',
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+              },
+              json: {
+                name: locations[i]
+              }
+            };
+
+            request(update, function(error, response, body) {
+              if (error) {
+                console.log(error);
+              } else {
+                res.send(body)
+              }
+            })
+          }
+        } else {
+          console.log('locations found');
         }
-       }
+      }
     })
   })
   .use(express.static(__dirname + '/'))
