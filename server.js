d9e2f5b2690f3cd0ebde86eac659d919fe5ee71f
Nguyen, Lyly <lyly.nguyen@accenture.com>
2016-11-14 09:38:58 -0800

    building mongoose model

--- server.js
+++ server.js
@@ -9,16 +9,15 @@ var request = require('request');
 var uri = process.env.MONGODB_URI || 'mongodb://lylyn:accenture1@ds151917.mlab.com:51917/persist-tasks';
 
 var Schema = new mongoose.Schema({
-  guid: String,
-  title: String,
-  username: String,
-  priority: String,
-  status: String,
-  description: String,
-  location: String,
-  creationTime: String,
-  deleted: Boolean
-
+    guid: String,
+    title: String,
+    username: String,
+    priority: String,
+    status: String,
+    description: String,
+    location: String,
+    creationTime: String,
+    deleted: Boolean
 });
 
 var Tasks = mongoose.model('Tasks', Schema);
@@ -31,6 +30,8 @@ mongoose.connect(uri, function (error) {
  }
 });
 
+var baseConnectWorkerURL = 'http://ec2-52-26-69-118.us-west-2.compute.amazonaws.com/api/v1/';
+
 express()
   // https://scotch.io/tutorials/use-expressjs-to-get-url-and-post-parameters
   .use(bodyParser.json()) // support json encoded bodies
@@ -40,10 +41,16 @@ express()
     res.json(200, {msg: 'OK'});
   })
   
-  .get('api/tasks', function (req, res) {
-
+  .get('/api/copytasks', function (req, res) {
+    request.get(baseConnectWorkerURL + 'tasks', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        body = JSON.parse(body);
+        for(var i = 0; i < body.length; i++) {
+          res.send(body);
+        }
+       }
+    })
   })
-
   .use(express.static(__dirname + '/'))
   .listen(process.env.PORT || 5000);
 
